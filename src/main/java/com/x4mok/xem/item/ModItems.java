7bcf7dfe335ca38a2993e7ae88281c258d84a2c6
Mr-BlueCat <jeremy@thecollinshouse.com>
2024-05-27 21:07:31 +0100

    initial

---
+++ src/main/java/com/x4mok/xem/item/ModItems.java
@@ -0,0 +1,29 @@
+package com.x4mok.xem.item;
+
+import com.x4mok.xem.XEM;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModItems {
+
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, XEM.MODID);
+
+    public static final RegistryObject<Item> DRAGONBLOOD = ITEMS.register("dragonblood",
+            () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MATERIALS)));
+    public static final RegistryObject<Item> DRAGONSCALE = ITEMS.register("dragonscale",
+            () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MATERIALS)));
+    public static final RegistryObject<Item> MAGICENDSTONE = ITEMS.register("magicendstone",
+            () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MATERIALS)));
+    public static final RegistryObject<Item> MAGICFRAME = ITEMS.register("magicframe",
+            () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MATERIALS)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+}
