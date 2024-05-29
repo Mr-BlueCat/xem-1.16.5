6c508b134b23d588f036a0d2d74d74f29a75e742
Mr-BlueCat <jeremy@thecollinshouse.com>
2024-05-29 16:46:10 +0100

    Created totem of undying craft + 3 crafts for the 3 parts of the totem, 3 new items bc of that, new stack limits, updated todo

--- src/main/java/com/x4mok/xem/item/ModItems.java
+++ src/main/java/com/x4mok/xem/item/ModItems.java
@@ -14,13 +14,19 @@ public class ModItems {
             DeferredRegister.create(ForgeRegistries.ITEMS, XEM.MODID);
 
     public static final RegistryObject<Item> DRAGONBLOOD = ITEMS.register("dragonblood",
-            () -> new Item(new Item.Properties().tab(ModItemGroup.XEMTAB)));
+            () -> new Item(new Item.Properties().tab(ModItemGroup.XEMTAB).stacksTo(16)));
     public static final RegistryObject<Item> DRAGONSCALE = ITEMS.register("dragonscale",
-            () -> new Item(new Item.Properties().tab(ModItemGroup.XEMTAB)));
+            () -> new Item(new Item.Properties().tab(ModItemGroup.XEMTAB).stacksTo(16)));
     public static final RegistryObject<Item> MAGICFRAME = ITEMS.register("magicframe",
-            () -> new Item(new Item.Properties().tab(ModItemGroup.XEMTAB)));
+            () -> new Item(new Item.Properties().tab(ModItemGroup.XEMTAB).stacksTo(16)));
     public static final RegistryObject<Item> TRIDENTHANDLE = ITEMS.register("tridenthandle",
-            () -> new Item(new Item.Properties().tab(ModItemGroup.XEMTAB)));
+            () -> new Item(new Item.Properties().tab(ModItemGroup.XEMTAB).stacksTo(16)));
+    public static final RegistryObject<Item> TOTEMHEAD = ITEMS.register("totemhead",
+            () -> new Item(new Item.Properties().tab(ModItemGroup.XEMTAB).stacksTo(2)));
+    public static final RegistryObject<Item> TOTEMCORE = ITEMS.register("totemcore",
+            () -> new Item(new Item.Properties().tab(ModItemGroup.XEMTAB).stacksTo(2)));
+    public static final RegistryObject<Item> TOTEMARM = ITEMS.register("totemarm",
+            () -> new Item(new Item.Properties().tab(ModItemGroup.XEMTAB).stacksTo(4)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
