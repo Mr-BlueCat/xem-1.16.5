33dd5eae058777398b7435c30f54e35fc28f1483
Mr-BlueCat <jeremy@thecollinshouse.com>
2024-05-31 12:56:39 +0100

    tags exist now! Added forge:ingots/copper, forge:ingots, xem:ingots/copper, and xem:ingots tags + all of them to the copper ingot

---
+++ src/main/java/com/x4mok/xem/util/ModTags.java
@@ -0,0 +1,48 @@
+package com.x4mok.xem.util;
+
+import com.x4mok.xem.XEM;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.Tags;
+
+public class ModTags {
+
+    public static class Blocks {
+
+        private static Tags.IOptionalNamedTag<Block> createTag(String name) {
+            return BlockTags.createOptional(new ResourceLocation(XEM.MODID, name));
+        }
+
+        private static Tags.IOptionalNamedTag<Block> createForgeTag(String name) {
+            return BlockTags.createOptional(new ResourceLocation("forge", name));
+        }
+
+
+    }
+
+    public static class Items {
+        public static final Tags.IOptionalNamedTag<Item> COPPER =
+                createTag("ingots/copper");
+
+        public static final Tags.IOptionalNamedTag<Item> FORGE_COPPER =
+                createForgeTag("ingots/copper");
+
+        public static final Tags.IOptionalNamedTag<Item> INGOT =
+                createTag("ingots");
+
+        public static final Tags.IOptionalNamedTag<Item> FORGE_INGOT =
+                createForgeTag("ingots");
+        private static Tags.IOptionalNamedTag<Item> createTag(String name) {
+            return ItemTags.createOptional(new ResourceLocation(XEM.MODID, name));
+        }
+
+        private static Tags.IOptionalNamedTag<Item> createForgeTag(String name) {
+            return ItemTags.createOptional(new ResourceLocation("forge", name));
+        }
+
+
+    }
+}
